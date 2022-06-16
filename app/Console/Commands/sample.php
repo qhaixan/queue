397de7dd1937fb71943a8e2ec8853d124fe8f19b
LAPTOP-EN93630V\Lor  QhaiXan <qhaixan.lor@diregaming.com>
2022-06-16 11:24:34 +0800

    first commit

---
+++ app/Console/Commands/sample.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Jobs\sampleJob;
+
+class sample extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'spam:log {count=100}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $count = $this->argument('count');
+        for ($i=0; $i < $count; $i++) { 
+            sampleJob::dispatch($i);
+        }
+        
+        $this->call('queue:work');
+        return 0;
+    }
+}
---
+++ app/Jobs/sampleJob.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+use Illuminate\Support\Facades\Log;
+class sampleJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    private $i;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($i)
+    {
+        $this->i = $i;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Log::info($this->i);
+    }
+}
